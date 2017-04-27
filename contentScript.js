103115cee78904da4477202f365c82cc511e384b
Sean Canton <eighteyes@gmail.com>
2017-04-27 13:01:03 -0700

    Wow

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,16 @@
 
 
 console.log('hi from Hemlock');
-chrome.runtime.sendMessage( {count : 666} );
+// chrome.runtime.sendMessage( {count : 666} );
+var tv = ['Game Of Thrones', 'Orange Is The New Black','The Big Bang Theory','Grey\'s Anatomy']
+var politics = ['Clinton', 'Trump', 'Democrat', 'Republican']
+var terror = ['ISIS', 'al-qaida', '9/11', '911']
+var tech = ['Zuckerberg','Steve Jobs','Elon Musk','Reed Hastings','Reid Hoffman','Peter Thiel','Jack Dorsey','Marc Andreessen','Larry Ellison','Tim Cook','Sergey Brin','Larry Page','Jeff Bezos']
+var crap = ['Kardashian', 'Selena Gomez', 'Stephen Colbert', 'Trevor Noah', 'Katy Perry', 'Seth Rogan', 'Jim Parsons', 'Mike Rowe', 'Neil Patrick Harris', 'Kayne','Simon Cowell', 'Hilton', 'Beyonce',
+'Robert Downey Jr.','Amy Schumer','Drake','Adele','Beyoncé', 'The Rock','Kanye','Nicki Minaj', 'Jennifer Lawrence',
+'Taylor Swift', 'Jenner', 'Nicolas Cage','Russell Brand', 'Miley Cyrus', 'Justin Bieber', 'Tom Cruise','Oprah'];
 
-var badList = ['Celebrities', 'Kardashian', 'Mike Rowe', 'Neil Patrick Harris', '92010', 'Kayne West','Simon Cowell', 'George Zimmerman', 'Paris Hilton', 'Beyonce', 'Obama', 'Zuckerberg', 'al-Qaida', 'Philip Seymour Hoffman', 'Russell Brand', 'Miley Cyrus', 'Justin Bieber', 'Tom Cruise'];
+var badList = [].concat(tv,politics,terror,crap,tech);
 var badUrls = [];
 badList.forEach( function(t, i, a) { badUrls[i] = t.replace(new RegExp(' ', 'g'), '-'); } );
 
@@ -25,42 +32,98 @@ function parseAttributes( domObj ){
   return false;
 }
 
+var count = 0;
+
 console.log("regex:", badNames, badUrls);
 matches = document.body.innerText.match(badNames);
 if (matches) {
-    var payload = {
-        count: matches.length // Pass the number of matches back.
-    };
-    chrome.runtime.sendMessage(payload, function(response) {
-      console.log("CS", response.farewell);
-    });
     // .sendRequest(payload, function(response) {});
     console.log('MATCH', matches);
+    runFilter();
 }
 
 
 // this is where they die
 //oh shit
 if ( location.href.match(badUrls) ) {
-  alert('Too deep already');
+  console.error('Too deep already');
 }
-// images
-$('img').filter( function() {
-  return parseAttributes( this );
-}).remove();
+$(function(){
+  setTimeout(runFilter, 10)
 
-//headlines and articles, be ruthless
-$('h1,h2,h3,h4,h5,h6,p,a,span').filter( function() {
-  return $(this).text().match(badNames);
-}).remove();
+})
 
-//links
-$('a').filter( function () {
-  return parseAttributes( this );
-}).remove();
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-//divs are a pain
-$('div').contents().filter(function() {
-  return this.nodeType === 3 && this.textContent.match(badNames);
-}).remove();
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    console.log(mutations, observer);
+    mutations.forEach(function(m){
+      if (m.type === 'attributes' && ( m.attributeName == 'href' || m.attributeName =='data-url') && m.target.tagName !== "HTML"){
+        if (!_.isNull(m.target.text.match(badUrls)) && m.target.text.match(badUrls).length > 0) {
+          removeTarget(m);
 
+        }
+      } else if (m.attributeName === 'class') {
+        if (!_.isNull(m.target.text.match(badNames) && m.target.text.match(badNames)){
+          removeTarget(m);
+        }
+      } else if ( !_.isNull(m.target.innerHTML.match(badUrls)) && !_.isNull(  m.target.innerText.match(badNames) ) && ( m.target.innerHTML.match(badUrls).length > 0 || m.target.innerText.match(badNames).length > 0) ){
+        if ( _.isNull(m.target.tagName.match(/BODY|SCRIPT/) )){
+          console.log(m.target, m.target.innerHTML.match(badUrls), m.target.innerText.match(badNames));
+          removeTarget(m);
+        }
+      }
+    })
+});
+
+function removeTarget(m){
+  try {
+    m.target.parentNode.removeChild(m.target);
+    count++;
+    chrome.runtime.sendMessage( {count : count} );
+  } catch (e){
+    console.error(m)
+  }
+}
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+  //...
+});
+
+function runFilter(){
+  // images
+  $('img').filter( function() {
+    return parseAttributes( this );
+  }).remove();
+
+  //headlines and articles, be ruthless
+  var l = $('h1,h2,h3,h4,h5,h6,p,a,span,strong').filter( function() {
+    return $(this).text().match(badNames);
+  }).remove();
+
+  count += l.length;
+
+  //links
+  var badLinks = $('a,div').filter( function () {
+    return parseAttributes( this );
+  });
+
+  badLinks.parent('article').remove();
+  badLinks.remove();
+
+  count += badLinks.length;
+
+  //divs are a pain
+  l = $('div').contents().filter(function() {
+    return this.nodeType === 3 && this.textContent.match(badNames);
+  }).remove();
+  count += l.length;
+
+
+  chrome.runtime.sendMessage( {count : count} );
+}
