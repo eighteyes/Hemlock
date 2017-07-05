3d250f544139608751a736d279cfb0a9d476f52c
Sean Canton <eighteyes@gmail.com>
2017-07-05 00:00:55 -0700

    the power

--- contentScript.js
+++ contentScript.js
@@ -1,26 +1,109 @@
 console.log('hi from Hemlock');
 // chrome.runtime.sendMessage( {count : 666} );
-var tv = ['Game Of Thrones', 'Orange Is The New Black', 'The Big Bang Theory', 'Grey\'s Anatomy']
-var politics = ['Clinton', 'Trump', 'Democrat', 'Republican', 'GOP','Jeff Sessions','Paul Ryan','Mike Pence']
+var tv = ['Game Of Thrones', 'Orange Is The New Black', 'The Big Bang Theory', 'Grey\'s Anatomy', 'NFL'];
+var politics = ['Clinton', 'Trump', 'Democrat', 'Republican', 'GOP','DNC', 'Jeff Sessions','Paul Ryan','Mike Pence'];
 var terror = ['ISIS']
-var tech = ['Zuckerberg', 'Steve Jobs', 'Elon Musk', 'Reed Hastings', 'Reid Hoffman', 'Peter Thiel', 'Jack Dorsey',
+var tech = ['Zuckerberg', 'Facebook', 'Steve Jobs', 'Elon Musk', 'Reed Hastings', 'Reid Hoffman', 'Peter Thiel', 'Jack Dorsey',
             'Marc Andreessen', 'Larry Ellison', 'Tim Cook', 'Sergey Brin', 'Larry Page', 'Jeff Bezos']
 var crap = ['Kardashian', 'Selena Gomez', 'Stephen Colbert', 'Trevor Noah', 'Katy Perry', 'Seth Rogan', 'Jim Parsons', 
-            'Mike Rowe', 'Neil Patrick Harris', 'Kayne', 'Simon Cowell', 'Hilton', 'Beyonce',
-  'Robert Downey Jr.', 'Amy Schumer', 'Drake', 'Adele', 'Beyoncé', 'Kanye', 'Nicki Minaj', 'Jennifer Lawrence',
+            'Mike Rowe', 'Neil Patrick Harris', 'Kayne', 'Simon Cowell', 'Hilton', 'Beyonce','Mark Cuban',
+  'Robert Downey', 'Amy Schumer', 'Drake', 'Adele', 'Beyoncé', 'Kanye', 'Nicki Minaj', 'Jennifer Lawrence',
   'Taylor Swift', 'Jenner', 'Nicolas Cage', 'Russell Brand', 'Miley Cyrus', 'Justin Bieber', 'Tom Cruise', 'Oprah',
             'Kushner','Ivanka'
 ];
 
-var badList = [].concat(tv, politics, terror, crap, tech);
-var badUrls = [];
-badList.forEach(function(t, i, a) { badUrls[i] = t.replace(new RegExp(' ', 'g'), '-'); });
+var badList = [].concat(tv, politics, terror, crap, tech) 
+// var bad = {
+//   list:[].concat(tv, politics, terror, crap, tech),
+//   urls: bad.list.forEach(function(t, i, a) { badUrls[i] = t.replace(new RegExp(' ', 'g'), '-'); }),
 
+
+// }
+var badUrls = [];
+badList.forEach
 var badRegexStr = badList.join('|');
 var badUrlsRegexStr = badUrls.join('|');
 
 var badNames = new RegExp(badRegexStr, "ig");
 var badUrls = new RegExp(badUrlsRegexStr, 'ig');
+
+
+chrome.storage.sync.get(null, function(names){
+  console.log('|=|_ > Blocks:', names);
+  
+  // if there ain't nothing use what we got
+  if ( !_.has(names, 'tech')){
+    chrome.storage.sync.set({
+      entertainment: [].concat(tv, terror, crap),
+      politics: politics,
+      tech: tech,
+    });
+  
+  }
+
+// otherwise use stored
+
+  badList = _.concat(names.entertainment, names.politics, names.tech);
+  console.log(badList, _.values(names))
+  badUrls = [];
+  
+  // replace spaces with - for urls
+  badList.forEach(function(t, i, a) { badUrls[i] = t.replace(new RegExp(' ', 'g'), '-'); });
+
+  badRegexStr = badList.join('|');
+  badUrlsRegexStr = badUrls.join('|');
+
+  badNames = new RegExp(badRegexStr, "ig");
+  badUrls = new RegExp(badUrlsRegexStr, 'ig');
+
+  var count = 0;
+
+  console.log("regex:", badNames, badUrls);
+  matches = document.body.innerText.match(badNames);
+  if (matches) {
+    // .sendRequest(payload, function(response) {});
+    console.log('MATCH', matches);
+    runBaseFilter();
+  }
+
+
+
+
+
+  // this is where they die
+  //oh shit
+  if (location.href.match(badUrls)) {
+    console.error('Too deep already');
+  }
+
+  // onready
+  $(function() {
+    setTimeout(runBaseFilter, 10)
+
+    var count = 0;
+
+    console.log("regex:", badNames, badUrls);
+    matches = document.body.innerText.match(badNames);
+    if (matches) {
+      // .sendRequest(payload, function(response) {});
+      console.log('MATCH', matches);
+      runBaseFilter();
+    }
+
+  })
+
+  // finish drawing
+  
+  // define what element should be observed by the observer
+  // and what types of mutations trigger the callback
+  observer.observe(document, {
+    subtree: true,
+    attributes: true
+      //...
+  });
+
+})
+
 
 // find badness in attributes
 function parseAttributes(domObj) {
@@ -34,50 +117,7 @@ function parseAttributes(domObj) {
   return false;
 }
 
-var count = 0;
 
-console.log("regex:", badNames, badUrls);
-matches = document.body.innerText.match(badNames);
-if (matches) {
-  // .sendRequest(payload, function(response) {});
-  console.log('MATCH', matches);
-  runFilter();
-}
-
-
-// this is where they die
-//oh shit
-if (location.href.match(badUrls)) {
-  console.error('Too deep already');
-}
-$(function() {
-  setTimeout(runFilter, 10)
-
-})
-
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-var observer = new MutationObserver(function(mutations, observer) {
-  // fired when a mutation occurs
-  console.log(mutations, observer);
-  mutations.forEach(function(m) {
-    if (m.type === 'attributes' && (m.attributeName == 'href' || m.attributeName == 'data-url') && m.target.tagName !== "HTML") {
-      if (!_.isNull(m.target.text.match(badUrls)) && m.target.text.match(badUrls).length > 0) {
-        removeTarget(m);
-
-      }
-    } else if (m.attributeName === 'class') {
-      if (!_.isNull(m.target.text.match(badNames)) && m.target.text.match(badNames)) {
-        removeTarget(m);
-      }
-    } else if (!_.isNull(m.target.innerHTML.match(badUrls)) && !_.isNull(m.target.innerText.match(badNames)) && (m.target.innerHTML.match(badUrls).length > 0 || m.target.innerText.match(badNames).length > 0)) {
-      if (_.isNull(m.target.tagName.match(/BODY|SCRIPT/))) {
-        console.log(m.target, m.target.innerHTML.match(badUrls), m.target.innerText.match(badNames));
-        removeTarget(m);
-      }
-    }
-  })
-});
 
 function removeTarget(m) {
   try {
@@ -89,15 +129,7 @@ function removeTarget(m) {
   }
 }
 
-// define what element should be observed by the observer
-// and what types of mutations trigger the callback
-observer.observe(document, {
-  subtree: true,
-  attributes: true
-    //...
-});
-
-function runFilter() {
+function runBaseFilter() {
   // images
   $('img').filter(function() {
     return parseAttributes(this);
--- panel.js
+++ panel.js
@@ -1,42 +1,53 @@
 console.log('hi from panel')
-alert('yo')
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
 xhr.open("GET", chrome.extension.getURL('/config.yaml'), true);
 xhr.send();
 
 function handleStateChange(data){
-  console.log(data);
+  console.log('Change', data);
 }
 function save_options() {
-  console.log('save')
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+  var o = {
+    entertainment: [],
+    tech: [],
+    politics: []
+  };
+  $('#tech input').each(function(i){ o.tech.push($(this).val()) });
+  $('#entertainment input').each(function(i){ o.entertainment.push($(this).val()) });
+  $('#politics input').each(function(i){ o.politics.push($(this).val()) });
+  console.log('update save', o);
+  chrome.storage.sync.set(o);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    blockList: {
-      "Politics":['Trump']
+  chrome.storage.sync.get(null, function(items) {
+    console.log('saved', items);
+    for ( var k in items){
+      var v = items[k];
+      var c = $('#'+k);
+      c.children('h1').prepend($('<button>+</button>&nbsp;').on('click', newName));    
+      v.forEach(function(name){
+        addInput(c, name);
+      })
     }
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
   });
+}
+
+var newName = function(){
+  addInput($(this).parent().parent(), '');
+}
+
+var addInput = function(c, name){
+  var input = $('<input type="text" value="'+ name + '">');
+  var btn = $('<button>x</button>').on('click', function(){
+    $(this).prev('input').remove();
+    $(this).remove();
+  });
+  c.append(input, btn);
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
